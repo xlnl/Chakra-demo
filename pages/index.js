ec4d38e8c36d22efd294ebeb4d40111c950ebb4a
mateenkazia <mateen.kazia@chaione.com>
2021-01-18 18:38:22 -0600

    starter file

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import { Box, Link, IconButton } from '@chakra-ui/core';
+
+const index = () => {
+	return (
+		<Box fontSize='2xl' textAlign='center'>
+			You can explore different examples from the navbar above ⬆️ <br />
+			Learn more about Chakr-UI @
+			<Link href='https://chakra-ui.com/'>www.chakra-ui.com</Link> <br />
+			Chakra-ui is made an maintaned by Segun Adebayo.
+			<br /> Segun Adebayo
+			<a href='https://twitter.com/thesegunadebayo' target='blank'>
+				<IconButton icon='at-sign' rounded='full' variantColor='blue' mx='3' />
+			</a>
+		</Box>
+	);
+};
+
+export default index;
